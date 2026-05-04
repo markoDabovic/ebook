cf10a66abc52586e1e8e5ae2fc9bf71f77555cc5
lazar saravolac <lazar.saravolac@gmail.com>
2026-05-04 22:03:56 +0200

    izmene preview

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,24 @@
 import { useTranslation } from "react-i18next";
+import engleskiPdf from "../assets/preview in english.pdf";
+import srpskiPdf from "../assets/preview in serbian.pdf";
+import knjigaSrpski from "../assets/book.jpg";
+import knjigaEngleski from "../assets/knjigaEngleski.jpg";
 
 export default function Product() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const openPdf = () => {
+    const pdfUrl = i18n.language === "sr" ? srpskiPdf : engleskiPdf;
+    window.open(pdfUrl, "_blank");
+  };
+
   return (
     <section id="program" className="py-12 bg-white ">
       <div className="max-w-6xl mx-auto flex flex-col gap-8 items-center">
-        <img src="/book.jpg" className="h-[400px] rounded-2xl shadow-xl ml-3" />
+        <img
+          src={i18n.language === "sr" ? knjigaSrpski : knjigaEngleski}
+          className="h-[400px] rounded-2xl shadow-xl ml-3"
+        />
 
         <div className="ml-4 mr-2">
           <h2 className="text-4xl font-bold mb-6 text-pink-300 font-oregano">
@@ -17,7 +30,10 @@ export default function Product() {
           </p>
 
           <div className="flex gap-5 flex align-center justify-center">
-            <button className="px-6 py-3 border w-[130px] border-pink-300 bg-pink-300 rounded-lg transition font-oregano">
+            <button
+              onClick={openPdf}
+              className="px-6 py-3 border w-[130px] border-pink-300 bg-pink-300 rounded-lg transition font-oregano"
+            >
               Preview
             </button>
             <button
